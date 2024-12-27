c49ab236212fa7d8666d064a8c4726b0a827df3d
Daniel Yonkeu <159975022+danielyonkeucheunko@users.noreply.github.com>
2024-12-26 19:39:04 -0500

    Added Error Handling for Binary Operators
    - Binary Operators with no left hand operands will now cause an error.

--- jlox/src/com/danielyonkeucheunko/lox/Parser.java
+++ jlox/src/com/danielyonkeucheunko/lox/Parser.java
@@ -111,6 +111,32 @@ public class Parser {
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
+
+        //Error Productions
+        if (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            error(previous(), "Missing left-hand operand.");
+            equality();
+            return null;
+        }
+
+        if (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            error(previous(), "Missing left-hand operand.");
+            comparison();
+            return null;
+        }
+
+        if (match(PLUS)) {
+            error(previous(), "Missing left-hand operand.");
+            term();
+            return null;
+        }
+
+        if (match(STAR, SLASH)) {
+            error(previous(), "Missing left-hand operand.");
+            factor();
+            return null;
+        }
+
         throw error(peek(), "Expect expression.");
     }
 
